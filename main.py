72674f512a8cedf8b5890eb354599e4f77c36b7c
jamnihil <jamcbritop@gmail.com>
2025-07-11 01:33:56 -0400

    -update_task method added

--- main.py
+++ main.py
@@ -11,8 +11,30 @@ class TaskManager:
         :param task: Task to be added
         :return: self.tasks
         """
-        task_id = len(self.tasks) + 1
+        task_id = str(len(self.tasks) + 1)
         self.tasks[task_id] = {'Description': task, 'Status': 'To-do',
                                'createdAt': '', 'updatedAt': '',}
 
         return self.tasks
+
+    def update_task(self, task_id, updated_task):
+        """
+
+        :param task_id:
+        :return:
+        """
+        if task_id in self.tasks:
+            self.tasks[task_id].update({'Description': updated_task,
+                                        'updatedAt': ''})
+            return self.tasks[task_id]
+        else:
+            print("Task ID does not exist")
+            return None
+
+tman = TaskManager()
+tman.add_task('Cocinar')
+print(tman.tasks)
+
+tman.update_task('1', 'Lavar')
+print(tman.tasks)
+
