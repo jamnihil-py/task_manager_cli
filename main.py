6d1488eae6217d68c2eec6b56b85da2dbfbb6544
jamnihil <jamcbritop@gmail.com>
2025-07-17 11:13:47 -0400

    -The software now keep running in CLI until the user press CTRL+C -Now add_task verify the task_id to avoid collisions.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import datetime
 import argparse
 import json
+import shlex
 from pathlib import Path
 
 TASKS_PATH = Path('tasks.json')
@@ -22,20 +23,23 @@ class TaskManager:
             with open(TASKS_PATH, 'w') as f:
                 f.write(json.dumps(self.tasks))
 
-
     def add_task(self, task):
         """
         Adds a task to the tasks dictionary
         :param task: Task to be added
         :return: self.tasks
         """
-        task_id = str(len(self.tasks) + 1)
+        task_id = len(self.tasks) + 1
+        while str(task_id) in self.tasks:
+            task_id += 1
+
         date_time = datetime.datetime.now()
 
-        self.tasks[task_id] = {'Description': task, 'Status': 'todo',
+        self.tasks[str(task_id)] = {'Description': task, 'Status': 'todo',
                                'createdAt': date_time.strftime('%c'),
                                'updatedAt': None,}
         self.save_json()
+        print(f"{task} successfully added")
         return self.tasks
 
     def update_task(self, task_id, new_task):
@@ -56,7 +60,7 @@ class TaskManager:
             self.save_json()
 
             if self.tasks[task_id]['Description'] == new_task:
-                print(f"'{old_task}' changed succesfully to '{new_task}'")
+                print(f"'{old_task}' changed successfully to '{new_task}'")
 
             return self.tasks[task_id]
 
@@ -74,7 +78,7 @@ class TaskManager:
 
         if task_id in self.tasks:
             deleted_task = self.tasks.pop(task_id)
-            print(f"'{deleted_task['Description']}' deleted succesfully")
+            print(f"'{deleted_task['Description']}' deleted successfully")
             self.save_json()
             return self.tasks
 
@@ -158,47 +162,64 @@ def main():
     """
     task_manager = TaskManager()
 
-    parser = argparse.ArgumentParser(description='Task Manager CLI')
-    subparser = parser.add_subparsers(dest='command', required=True)
+    while True:
+        try:
+            print(task_manager.tasks)
+            user_input = input("> ").strip()
+            if not user_input:
+                continue
 
-    #Add command
-    add_parser = subparser.add_parser('add')
-    add_parser.add_argument('description', help='Task description')
+            parts = shlex.split(user_input)
 
-    #Update command
-    update_parser = subparser.add_parser('update')
-    update_parser.add_argument('task_id', type=int, help='Task ID to update')
-    update_parser.add_argument('update', help='New task')
+            parser = argparse.ArgumentParser(description='Task Manager CLI')
+            subparser = parser.add_subparsers(dest='command', required=True)
 
-    #Delete command
-    delete_parser = subparser.add_parser('delete')
-    delete_parser.add_argument('task_id', type=int, help='Task ID to be deleted')
+            #Add command
+            add_parser = subparser.add_parser('add')
+            add_parser.add_argument('description', help='Task description')
 
-    #Mark status command
-    mark_parser = subparser.add_parser('mark')
-    mark_parser.add_argument('task_id', type=int, help='Task ID to change status')
-    mark_parser.add_argument('status',
-                                      choices=['todo', 'in-progress', 'done'],
-                                      help='New Status')
+            #Update command
+            update_parser = subparser.add_parser('update')
+            update_parser.add_argument('task_id', type=int, help='Task ID to update')
+            update_parser.add_argument('update', help='New task')
 
-    #List command
-    list_parser = subparser.add_parser('list')
-    list_parser.add_argument('status', nargs='?',
-                             choices=['todo', 'in-progress', 'done'],
-                             help='Filter by status')
+            #Delete command
+            delete_parser = subparser.add_parser('delete')
+            delete_parser.add_argument('task_id', type=int, help='Task ID to be deleted')
 
-    ###
-    args = parser.parse_args()
+            #Mark status command
+            mark_parser = subparser.add_parser('mark')
+            mark_parser.add_argument('task_id', type=int, help='Task ID to change status')
+            mark_parser.add_argument('status',
+                                              choices=['todo', 'in-progress', 'done'],
+                                              help='New Status')
 
-    if args.command == 'add':
-        task_manager.add_task(args.description)
-    elif args.command == 'update':
-        task_manager.update_task(args.task_id, args.update)
-    elif args.command == 'delete':
-        task_manager.delete_task(args.task_id)
-    elif args.command == 'mark':
-        task_manager.mark_status(args.task_id, args.status)
-    elif args.command == 'list':
-        task_manager.list(args.status)
+            #List command
+            list_parser = subparser.add_parser('list')
+            list_parser.add_argument('status', nargs='?',
+                                     choices=['todo', 'in-progress', 'done'],
+                                     help='Filter by status')
 
-main()
+            ###
+            args = parser.parse_args(parts)
+
+            if args.command == 'add':
+                task_manager.add_task(args.description)
+            elif args.command == 'update':
+                task_manager.update_task(str(args.task_id), args.update)
+            elif args.command == 'delete':
+                task_manager.delete_task(str(args.task_id))
+            elif args.command == 'mark':
+                task_manager.mark_status(str(args.task_id), args.status)
+            elif args.command == 'list':
+                task_manager.list(args.status)
+
+        except KeyboardInterrupt:
+            print("Exit")
+            break
+
+        except SystemExit:
+            continue
+
+if __name__ == '__main__':
+    main()
